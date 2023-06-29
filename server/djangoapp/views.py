0bbe6cd5d1ec47cf00c860a3627e9cd27a5f10c2
kaitoukuga <greaper747@gmail.com>
2023-06-29 19:33:34 -0400

    add static pages

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,12 +18,17 @@ logger = logging.getLogger(__name__)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/about.html', context)
 
 
 # Create a `contact` view to return a static contact page
-# def contact(request):
+def contact(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
@@ -45,16 +50,16 @@ def get_dealerships(request):
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-def get_dealer_details(request, dealer_id):
-    context = {}
-    # If the request method is GET
-    if request.method == 'GET':
-        # Using the objects model manage to read all dealer list
-        # and sort them by total_review descending
-        dealer_list = Dealer.objects.order_by('-total_review')[:10]
-        # Appen the dealer list as an entry of context dict
-        context['dealer_list'] = dealer_list
-        return render(request, 'djangoapp/dealer_list.html', context)
+# def get_dealer_details(request, dealer_id):
+#     context = {}
+#     # If the request method is GET
+#     if request.method == 'GET':
+#         # Using the objects model manage to read all dealer list
+#         # and sort them by total_review descending
+#         dealer_list = Dealer.objects.order_by('-total_review')[:10]
+#         # Appen the dealer list as an entry of context dict
+#         context['dealer_list'] = dealer_list
+#         return render(request, 'djangoapp/dealer_list.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
